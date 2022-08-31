f9d95ed9e2a15c7548e78a76fd716713d75da418
Kristjan <kristjan@lendit.ee>
2022-09-01 00:02:24 +0300

    v3.4 interface change, code cleanup, adding next tile action drawing, fixed bugs

--- src/eChinBot/eMain.java
+++ src/eChinBot/eMain.java
@@ -1,13 +1,11 @@
 package eChinBot;
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.text.DecimalFormat;
+import java.awt.*;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.concurrent.TimeUnit;
 
 import net.runelite.api.coords.WorldPoint;
+import simple.hooks.filters.SimpleSkills;
 import simple.hooks.filters.SimpleSkills.Skills;
 import simple.hooks.scripts.Category;
 import simple.hooks.scripts.ScriptManifest;
@@ -16,219 +14,260 @@ import simple.hooks.wrappers.SimpleGroundItem;
 import simple.hooks.wrappers.SimpleItem;
 import simple.hooks.wrappers.SimpleObject;
 import simple.robot.script.Script;
-import simple.robot.utils.ScriptUtils;
 
-@ScriptManifest(author = "Nate/Trester/Esmaabi", category = Category.HUNTER, description = "Start near chins with boxes in inv, rework by Trester & Esmaabi",
-discord = "Nathan#6809 | Loreen#4582 | Esmaabi#5752", name = "Amazing Chins v3", servers = { "Zaros, OSRSPS" }, version = "3.1")
+@ScriptManifest(author = "Nate/Trester/Esmaabi", category = Category.HUNTER, description = "Start near chinchompa hunting spot with boxes in inventory. Script reworked by Trester & Esmaabi",
+        discord = "Nathan#6809 | Loreen#4582 | Esmaabi#5752", name = "Amazing Chins v3", servers = {"Zaros, OSRSPS"}, version = "3.4")
 
-public class eMain extends Script{
+public class eMain extends Script {
 
-public final int BOX_TRAP_ITEM = 10008;
-public WorldPoint startingTile;
-public DecimalFormat formatter= new DecimalFormat("#,###,###,###");
-public String status;
-public int startExperience;
-public int chinsGained;
-public long startTime;
-public long timeRan;
+    public final int BOX_TRAP_ITEM = 10008;
+    public WorldPoint startingTile;
+    public String status;
+    public int chinsGained;
+    boolean trapsPickup = false;
+    private long startTime = 0L;
+    private long startingSkillLevel;
+    private long startingSkillExp;
+    private WorldPoint[] locs = null;
 
-private WorldPoint[] locs = null;
+    public static String currentTime() {
+        return LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm"));
+    }
 
-public static String currentTime() {
-		return LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm"));
-	}
+    @Override
+    public void paint(Graphics g) {
 
-@Override
-public void paint(Graphics g) {
-	timeRan = System.currentTimeMillis() - startTime;
-	g.setColor(Color.BLACK);
-    g.fillRect(5, 230, 200, 70);
-	g.setColor(Color.ORANGE);
-    g.drawRect(5, 230, 200, 70);
-    g.drawString("Amazing Chins v3", 7, 245);
-    g.drawString("Time Ran: " + ft(timeRan), 7, 260);
-    g.drawString("Exp: " + formatter.format(this.getGainedXP()) + " (" + formatter.format(ScriptUtils.getValuePerHour(startTime, System.currentTimeMillis(), getGainedXP())) + ")", 7, 275);
-    g.drawString("Chins: " + formatter.format(this.getGainedChins()) + " (" + formatter.format(ScriptUtils.getValuePerHour(startTime, System.currentTimeMillis(), getGainedChins())) + ")", 7, 290);
+        Color PhilippineRed = new Color(196, 18, 48);
+        Color RaisinBlack = new Color(35, 31, 32, 127);
+        g.setColor(RaisinBlack);
+        g.fillRect(5, 120, 200, 110);
+        g.setColor(PhilippineRed);
+        g.drawRect(5, 120, 200, 110);
+        g.setColor(PhilippineRed);
+        g.drawString("Amazing Chins v3", 15, 135);
+        g.setColor(Color.WHITE);
+        long runTime = System.currentTimeMillis() - this.startTime;
+        long currentSkillLevel = this.ctx.skills.realLevel(Skills.HUNTER);
+        long currentSkillExp = this.ctx.skills.experience(SimpleSkills.Skills.HUNTER);
+        long SkillLevelsGained = currentSkillLevel - this.startingSkillLevel;
+        long SkillExpGained = currentSkillExp - this.startingSkillExp;
+        long SkillExpPerHour = (int) ((SkillExpGained * 3600000D) / runTime);
+        long ThingsPerHour = (int) (getGainedChins() / ((System.currentTimeMillis() - this.startTime) / 3600000.0D));
+        g.drawString("Runtime: " + formatTime(runTime), 15, 150);
+        g.drawString("Starting Level: " + this.startingSkillLevel + " (+" + SkillLevelsGained + ")", 15, 165);
+        g.drawString("Current Level: " + currentSkillLevel, 15, 180);
+        g.drawString("Exp gained: " + SkillExpGained + " (" + (SkillExpPerHour / 1000L) + "k" + " xp/h)", 15, 195);
+        g.drawString("Chins caught: " + getGainedChins() + " (" + ThingsPerHour + " chins/h)", 15, 210);
+        g.drawString("Status: " + status, 15, 225);
 
-/*	if(locs != null) {
-		ctx.paint.drawTileMatrix(g, locs, Color.CYAN);
-	}*/
 
-}
+        //highlight next tile where box trap will be interacted with
+        if (locs != null && getAvailableTrapLocation() != null) {
+            for (int i = 0; i < placedTraps(); i++) {
+                if (i < placedTraps()) {
+                    ctx.paint.drawTileMatrix((Graphics2D) g, getAvailableTrapLocation(), Color.GREEN);
+                }
+            }
+        }
 
-@Override
-public void onChatMessage(ChatMessage m) {
-	if (m.getMessage() != null) {
-		String message = m.getMessage().toLowerCase();
-		if (message.contains(ctx.players.getLocal().getName().toLowerCase())) {
-			ctx.updateStatus(currentTime() + " Someone asked for you");
-			ctx.updateStatus(currentTime() + " Stopping script");
-			ctx.stopScript();
-		}
-	}
-}
+/*        SimpleObject trap = ctx.objects.populate().filter(9380, 9382, 9385, 9384).filter(t -> objectInLocation(t.getLocation())).next();
+		if (locs != null && trap != null) {
+				ctx.paint.drawTileMatrix((Graphics2D) g, trap.getLocation(), Color.YELLOW);
+		}*/
 
-@Override
-public void onExecute() {
-	//ctx.updateStatus("Starting Perfect Chin Catcher");
-	startTime = System.currentTimeMillis();
-	chinsGained = 0;
-	startExperience = ctx.skills.experience(Skills.HUNTER);
-	startingTile = ctx.players.getLocal().getLocation();
-	int p = ctx.players.getLocal().getLocation().getPlane();
-	locs = new WorldPoint[] {
-			new WorldPoint(startingTile.getX(), startingTile.getY(), p),
-			new WorldPoint(startingTile.getX() - 1, startingTile.getY() + 1, p),
-			new WorldPoint(startingTile.getX() + 1, startingTile.getY() + 1, p),
-			new WorldPoint(startingTile.getX() + 1, startingTile.getY() - 1, p),
-			new WorldPoint(startingTile.getX() - 1, startingTile.getY() - 1, p)
-	};
-}
 
-@Override
-public void onProcess() {
-	if(ctx.players.getLocal().getAnimation() != -1) {
-		ctx.onCondition(() -> ctx.players.getLocal().getAnimation() == -1, 200,10);
-		ctx.sleep(500);
-		return;
-	}
-	//ctx.updateStatus("Scanning for Traps");
-	SimpleGroundItem floorTrap = ctx.groundItems.populate().filter(BOX_TRAP_ITEM).nearest().next();
-	if(floorTrap != null && floorTrap.validateInteractable() && !ctx.pathing.inMotion()) {
-		//ctx.updateStatus("Picking up broken trap");
-		int trapAm = ctx.inventory.populate().filter(BOX_TRAP_ITEM).population();
-		if(floorTrap.click("lay")) {
-			ctx.sleep(1000);
-			ctx.onCondition(() -> ctx.inventory.populate().filter(BOX_TRAP_ITEM).population() > trapAm,250,5);
-		}
-		return;
-	}
-	WorldPoint trapTile = getAvailableTrapLocation();
-	if(placedTraps() != trapAmount() && trapTile != null) {
-		//ctx.updateStatus("Trap is in inventory");
-		SimpleItem invTrap = ctx.inventory.populate().filter(BOX_TRAP_ITEM).next();
+    }
 
-		if(invTrap != null && trapTile != null) {
+    @Override
+    public void onChatMessage(ChatMessage m) {
+        if (m.getMessage() != null) {
+            String message = m.getMessage().toLowerCase();
+            if (message.contains(ctx.players.getLocal().getName().toLowerCase())) {
+                ctx.updateStatus(currentTime() + " Someone asked for you");
+                ctx.updateStatus(currentTime() + " Stopping script");
+                ctx.stopScript();
+            } else if (message.contains("don't have a high enough hunter")) {
+                status = "Too many traps!";
+                trapsPickup = true;
+            } else if (message.contains("can't lay a trap here")) {
+                status = "Can't lay here!";
+                trapsPickup = true;
+            }
+        }
+    }
 
-			if(!ctx.players.getLocal().getLocation().equals(trapTile)) {
-				//ctx.updateStatus("Walking to available trap spot");
-				ctx.pathing.step(trapTile.getX(),trapTile.getY());
-				ctx.onCondition(()-> ctx.players.getLocal().getLocation().equals(trapTile),200,10);
-			}
-			if(ctx.players.getLocal().getLocation().equals(trapTile)) {
-				//ctx.updateStatus("Setting up trap");
-				setupTrap(invTrap, trapTile);
-			}
-		} else {
-			//System.out.println("Shit is nulled");
-		}
-	} else {
-	//	//ctx.updateStatus("Scanning ground objects for traps");
-		SimpleObject trap = ctx.objects.populate().filter(9382,9385,9384).filter(t -> objectInLocation(t.getLocation())).next();
-		if(trap != null && trap.validateInteractable() && !ctx.pathing.inMotion()){
-		//	//ctx.updateStatus("Resetting traps");
-			int chins = getChinCount();
-			if(trap.click("Reset")){
-				if(trap.getName().toLowerCase().equals("shaking box") && ctx.onCondition(()-> getChinCount() > chins,250,10)){
-					chinsGained++;
-				}
-				ctx.onCondition(()-> !trapExistsForTile(trap.getLocation()));
-			}
-		}
-	}
-}
+    @Override
+    public void onExecute() {
+        status = "Setting up bot";
+        //vars
+        this.startTime = System.currentTimeMillis(); //paint
+        this.startingSkillLevel = this.ctx.skills.realLevel(Skills.HUNTER);
+        this.startingSkillExp = this.ctx.skills.experience(Skills.HUNTER);
+        chinsGained = 0;
 
-private boolean objectInLocation(WorldPoint w)
-{
-	for (int i = 0; i < locs.length; i++) {
-		if (w.distanceTo(locs[i]) == 0) {
-			return true;
-		}
-	}
-	return false;
-}
+        // viewport settings
+        ctx.viewport.angle(0);
+        ctx.viewport.pitch(true);
 
-public WorldPoint getAvailableTrapLocation(){
-	for (int i = 0; i < trapAmount(); i++) {
-		WorldPoint loc = locs[i];
-		if (!trapExistsForTile(loc)) {
-			return loc;
-		}
-	}
-	return null;
-}
+        //traps location
+        startingTile = ctx.players.getLocal().getLocation();
+        int p = ctx.players.getLocal().getLocation().getPlane();
+        locs = new WorldPoint[]{
+                new WorldPoint(startingTile.getX(), startingTile.getY(), p),
+                new WorldPoint(startingTile.getX() - 1, startingTile.getY() + 1, p),
+                new WorldPoint(startingTile.getX() + 1, startingTile.getY() + 1, p),
+                new WorldPoint(startingTile.getX() + 1, startingTile.getY() - 1, p),
+                new WorldPoint(startingTile.getX() - 1, startingTile.getY() - 1, p)
+        };
+    }
 
-public int trapAmount() {
-	final int level = ctx.skills.level(Skills.HUNTER);
-	if (level >= 80) {
-		return 5;
-	} else if (level >= 60) {
-		return 4;
-	} else if (level >= 40) {
-		return 3;
-	} else if (level >= 20) {
-		return 2;
-	}
-	return 1;
-}
+    @Override
+    public void onProcess() {
+        if (ctx.players.getLocal().getAnimation() != -1) {
+            ctx.onCondition(() -> ctx.players.getLocal().getAnimation() == -1, 200, 10);
+            ctx.sleep(500);
+            return;
+        }
 
-public int getGainedXP() {
-	return this.ctx.skills.experience(Skills.HUNTER) - startExperience;
-}
+        if (!trapsPickup) {
 
-public int getGainedChins() {
-	return chinsGained;
-}
+            SimpleGroundItem floorTrap = ctx.groundItems.populate().filter(BOX_TRAP_ITEM).nearest().next();
+            if (floorTrap != null && floorTrap.validateInteractable() && !ctx.pathing.inMotion() && !ctx.players.getLocal().isAnimating()) {
+                status = "Laying trap";
+                int trapAm = ctx.inventory.populate().filter(BOX_TRAP_ITEM).population();
+                if (floorTrap.click("Lay", "Box trap")) {
+                    ctx.sleep(1000);
+                    ctx.onCondition(() -> ctx.inventory.populate().filter(BOX_TRAP_ITEM).population() > trapAm, 1200);
+                }
+                return;
+            }
+            WorldPoint trapTile = getAvailableTrapLocation();
+            if (placedTraps() != trapAmount() && trapTile != null) {
+                status = "Checking traps in inventory";
+                SimpleItem invTrap = ctx.inventory.populate().filter(BOX_TRAP_ITEM).next();
 
-public int getChinCount() {
-	return ctx.inventory.populate().filter("chinchompa", "red chinchompa", "black chinchompa", "crystal chinchompa").population(true);
-}
+                if (invTrap != null && trapTile != null) {
 
-public void setupTrap(final SimpleItem invTrap, final WorldPoint tile) {
-	if (invTrap.click(1)) {
-		ctx.sleepCondition(() -> this.trapExistsForTile(tile));
-	}
-}
+                    if (!ctx.players.getLocal().getLocation().equals(trapTile)) {
+                        status = "Walking to next spot";
+                        ctx.pathing.step(trapTile.getX(), trapTile.getY());
+                        ctx.onCondition(() -> ctx.players.getLocal().getLocation().equals(trapTile), 200, 10);
+                    }
+                    if (ctx.players.getLocal().getLocation().equals(trapTile)) {
+                        status = "Setting up trap";
+                        setupTrap(invTrap, trapTile);
+                    }
+                } else {
+                    status = "Error -> traps nulled...";
+                }
+            } else {
+                status = "Waiting for action";
+                SimpleObject trap = ctx.objects.populate().filter(9382, 9385, 9384).filter(t -> objectInLocation(t.getLocation())).next();
+                if (trap != null && trap.validateInteractable() && !ctx.pathing.inMotion() && !ctx.players.getLocal().isAnimating()) {
+                    status = "Resetting traps";
+                    int chins = getChinCount();
+                    if (trap.click("Reset")) {
+                        if (trap.getName().equalsIgnoreCase("shaking box") && ctx.onCondition(() -> getChinCount() > chins, 2400)) {
+                            chinsGained++;
+                        }
+                        ctx.onCondition(() -> !trapExistsForTile(trap.getLocation()));
+                    }
+                }
+            }
+        } else {
+            if (ctx.groundItems.populate().filter(BOX_TRAP_ITEM).population() > 0) {
+                SimpleGroundItem trapsGround = ctx.groundItems.populate().filter(BOX_TRAP_ITEM).nearest().next();
+                status = "Picking up trap(s)";
+                int trapsInv = getTrapCountInv();
+                if (trapsGround != null && trapsGround.validateInteractable() && !ctx.pathing.inMotion() && !ctx.players.getLocal().isAnimating()) {
+                    trapsGround.click("Take", "Box trap");
+                    ctx.onCondition(() -> ctx.inventory.populate().filter("Box trap").population() > trapsInv, 1200);
+                }
+            } else {
+                status = "No traps to pickup";
+                trapsPickup = false;
+            }
+        }
+    }
 
-public boolean trapExistsForTile(final WorldPoint tile) {
-	return !ctx.objects.populate().filter(9380, 9382,9385,9384).filter(tile).isEmpty();
-}
+    private boolean objectInLocation(WorldPoint w) {
+        for (WorldPoint loc : locs) {
+            if (w.distanceTo(loc) == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-public int placedTraps() {
-	int count = 0;
-	for (int i = 0; i < trapAmount(); i++) {
-		WorldPoint loc = locs[i];
-		if (trapExistsForTile(loc)) {
-			count++;
-		}
-	}
-	return count;
-}
+    public WorldPoint getAvailableTrapLocation() {
+        for (int i = 0; i < trapAmount(); i++) {
+            WorldPoint loc = locs[i];
+            if (!trapExistsForTile(loc)) {
+                return loc;
+            }
+        }
+        return null;
+    }
 
-@Override
-public void onTerminate() {
-	// TODO Auto-generated method stub
+    public int trapAmount() {
+        final int level = ctx.skills.level(Skills.HUNTER);
+        if (level >= 80) {
+            return 5;
+        } else if (level >= 60) {
+            return 4;
+        } else if (level >= 40) {
+            return 3;
+        } else if (level >= 20) {
+            return 2;
+        }
+        return 1;
+    }
 
-}
+    public int getGainedChins() {
+        return chinsGained;
+    }
 
-private String ft(long duration) {
-	String res = "";
-	long days = TimeUnit.MILLISECONDS.toDays(duration);
-	long hours = TimeUnit.MILLISECONDS.toHours(duration)
-			- TimeUnit.DAYS.toHours(TimeUnit.MILLISECONDS.toDays(duration));
-	long minutes = TimeUnit.MILLISECONDS.toMinutes(duration)
-			- TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS
-					.toHours(duration));
-	long seconds = TimeUnit.MILLISECONDS.toSeconds(duration)
-			- TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS
-					.toMinutes(duration));
-	if (days == 0) {
-		res = (hours + ":" + minutes + ":" + seconds);
-	} else {
-		res = (days + ":" + hours + ":" + minutes + ":" + seconds);
-	}
-	return res;
+    public int getChinCount() {
+        return ctx.inventory.populate().filter("chinchompa", "red chinchompa", "black chinchompa", "crystal chinchompa").population(true);
+    }
 
-}
+    public int getTrapCountInv() {
+        return ctx.inventory.populate().filter("Box trap").population();
+    }
+
+    public void setupTrap(final SimpleItem invTrap, final WorldPoint tile) {
+        if (invTrap.click(1)) {
+            ctx.sleepCondition(() -> this.trapExistsForTile(tile));
+        }
+    }
+
+    public boolean trapExistsForTile(final WorldPoint tile) {
+        return !ctx.objects.populate().filter(9380, 9382, 9385, 9384).filter(tile).isEmpty();
+    }
+
+    public int placedTraps() {
+        int count = 0;
+        for (int i = 0; i < trapAmount(); i++) {
+            WorldPoint loc = locs[i];
+            if (trapExistsForTile(loc)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    @Override
+    public void onTerminate() {
+    }
+
+    private String formatTime(long ms) {
+        long s = ms / 1000L;
+        long m = s / 60L;
+        long h = m / 60L;
+        s %= 60L;
+        m %= 60L;
+        h %= 24L;
+        return String.format("%02d:%02d:%02d", h, m, s);
+    }
 
 }
